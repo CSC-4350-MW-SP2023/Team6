074933d60514ecd1bf9f8477c7419f11d780a97d
satvik-1203 <satvikgsu@gmail.com>
2023-03-06 14:34:38 -0500

    changed login settings

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import type { NextPage } from "next";
+import { redirect } from "next/dist/server/api-utils";
 
 const Login: NextPage = () => {
   const supabaseClient = useSupabaseClient();
@@ -9,7 +10,11 @@ const Login: NextPage = () => {
   if (!user)
     return (
       <Auth
-        redirectTo="http://localhost:3000/"
+        redirectTo={
+          process.env.NODE_ENV == "production"
+            ? "https://porthub-two.vercel.app/"
+            : "http://localhost:3000/"
+        }
         supabaseClient={supabaseClient}
         appearance={{ theme: ThemeSupa }}
         providers={["google"]}
